672d3bd1be4956da6b489e549ccccc80aab6f845
vasilverio <vasco123silverio@gmail.com>
2023-03-27 22:40:05 +0100

    Criação de Projecto

---
+++ Eq2Grau/Eq2Grau/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eq2Grau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
